6d7f0f6881ba5f26d89072caaa28d8ac23aba307
katerennie97 <katerennie@outlook.com.au>
2023-01-17 20:26:38 +1100

    some commit

--- crowdfunding/crowdfunding/urls.py
+++ crowdfunding/crowdfunding/urls.py
@@ -18,5 +18,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('projects.urls'),)
+    path('', include('projects.urls'),),
+    path('users/', include('users.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
+
+
